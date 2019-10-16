6821f4d2011253ed73c9211b87296bdc10a3a2b3
Dakrs <diogosobral98@gmail.com>
2019-10-16 09:59:05 +0100

    Final 2.0

---
+++ HealthcareSensor/src/Models/Patient.py
@@ -0,0 +1,36 @@
+from datetime import date
+
+class Patient:
+    def __init__(self, patientid,name,birthdate,age,sensorid):
+        self.patientid = patientid
+        self.name = name
+        self.birthdate = birthdate
+        self.age = age
+        self.sensorid = sensorid
+
+    def getId(self):
+        return self.patientid
+    
+    def getName(self):
+        return self.name
+    
+    def getBirthDate(self):
+        return self.birthdate
+    
+    def getAge(self):
+        return self.age
+
+    def getSensorId(self):
+        return self.sensorid
+
+    def insertQuery(self):
+        return ("INSERT INTO Patient VALUES (%s,%s,%s,%s,%s)")
+
+    def verifyQuery(self):
+        return ("SELECT * FROM Patient WHERE patientid = %s")
+    
+    def data(self):
+        return (self.patientid,self.name,self.birthdate,self.age,self.sensorid)
+    
+    def logs(self):
+        print(f"Inseri paciente com id {self.patientid}")
